7be7c887b98b6e1952549953026f0458a4f5a097
Lanao-hub <oudianelionel@gmail.com>
2026-05-11 22:37:52 +0000

    Update lionel.py

--- lionel.py
+++ lionel.py
@@ -1,33 +1,36 @@
 import streamlit as st
 
 
-if st.sidebar.button("Accueil",use_container_width=True):   
-    st.title("Lionel Albert Nandy Amar OUDIANE")
+   
+st.title("Lionel Albert Nandy Amar OUDIANE")
+st.header("A propos de moi")
+st.markdown("""Technicien supérieur en production végétale et animale. Souhaitant exprimer ses compétences dans le domaine agronomique à travers les plus grands de ce domaine """)
 
-    with st.expander("PROFIL"):
-        st.write("Date de Naissance: 10/10/2005")
-        st.write("Addresse: Gandigal,Mbour")
-        st.markdown("""Technicien en production végétale et animale. Souhaitant """)
+st.subheader("INFO")
+st.write("Date de Naissance: 10/10/2005")
+st.write("Addresse: Gandigal,Mbour")        
 
-    with st.expander("PARCOURS"):
-        st.write("Licence en science agricoles, alimentaires et nutritionnelles")
-        st.write("Baccalaurioat Scientifique")
+st.subheader("PARCOURS")
+st.write("Licence en science agricoles, alimentaires et nutritionnelles")
+st.write("Baccalaurioat Scientifique")
         
-    with st.expander("COMPETENCE"):
-        st.write("Gérance d'une exploitation agricole")
-        st.write("Gérance d'une exploitation porcine et de volaille")
-        st.write("Bureautique(Word,Excel,Powerpoint)")
-if st.sidebar.button("Projet",use_container_width=True):
-    st.header("Projet")
+st.header("Compétence")
+st.write("Gestion d'une exploitation agricole")
+st.write("Gestion d'une exploitation porcine et de volaille")
+st.write("Analyse de sols")
+st.write("Nutrition et développement des plantes")
+st.write("Bureautique(Word,Excel,Powerpoint)")
+
+st.header("Projet")
+with st.expander("Cliquer pour en savoir plus"):
+    
     st.write("Gestion d'un poulailler")
-    with st.expander("Description"):
-        st.markdown("""Gérer un poulailler de 100 poulets sur une période de 45 jours. Au terme duquel on a eu une perte de 5 poulets et le reste vendu 4000 FCFA""")
-    with st.expander("Média"):
-        st.image("1.jpg")
-        st.image("2.jpg")
-if st.sidebar.button("Contact", use_container_width=True):
-    st.subheader("Contact")
-    with st.expander("Contact"):
-        st.write("Contact: +221 77 015 78 86")
-    with st.expander("E-mail"):
-        st.markdown("oudianelionel@gmail.com")
+    st.markdown("""Gérer un poulailler de 100 poulets sur une période de 45 jours. Au terme duquel on a eu une perte de 5 poulets et le reste vendu 4000 FCFA l'unité""")
+    st.image("1.jpg")
+    st.image("2.jpg")
+
+with st.sidebar:
+    st.title("Contact")
+    
+    st.markdown("Mail: oudianelionel@gmail.com")
+    st.markdown("Réactif et disponible, je réponds généralement à vos messages en moins de quelques heures.")
